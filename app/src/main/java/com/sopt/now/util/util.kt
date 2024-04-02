729802f667ad5a855a614fccac119ed39603d2a0
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-03 04:16:01 +0900

    getParcelable 유틸 추가
    parcelize 사용을 위해 버전에 따른 분기

---
+++ app/src/main/java/com/sopt/now/util/util.kt
@@ -0,0 +1,9 @@
+package com.sopt.now.util
+
+import android.content.Intent
+import android.os.Build
+
+fun <T> Intent.getParcelable(key: String, c: Class<T>) = when {
+    Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU -> getParcelableExtra(key, c)
+    else -> getParcelableExtra(key)
+}
