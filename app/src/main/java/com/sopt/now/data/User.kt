68f0545858bf9c3d1619e45bdb765019f84cf670
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-03 04:14:37 +0900

    회원정보 데이터클래스 추가

---
+++ app/src/main/java/com/sopt/now/data/User.kt
@@ -0,0 +1,12 @@
+package com.sopt.now.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val id: String,
+    val pw: String,
+    val nickname: String,
+    val mbti: String
+) : Parcelable
