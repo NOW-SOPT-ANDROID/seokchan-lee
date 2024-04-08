7c352a2c200c5db5d420bbbdaab8dd6ae9655bae
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-08 19:28:38 +0900

    companion object 코드 하단으로 변경
    코드리뷰

--- app/src/main/java/com/sopt/now/MyApplication.kt
+++ app/src/main/java/com/sopt/now/MyApplication.kt
@@ -3,13 +3,14 @@ package com.sopt.now
 import android.app.Application
 import com.sopt.now.util.preferenceUtil
 
-class MyApplication:Application() {
-    companion object{
-        lateinit var userdata : preferenceUtil
-    }
+class MyApplication : Application() {
 
     override fun onCreate() {
         userdata = preferenceUtil(applicationContext)
         super.onCreate()
+    }
+
+    companion object {
+        lateinit var userdata: preferenceUtil
     }
 }
