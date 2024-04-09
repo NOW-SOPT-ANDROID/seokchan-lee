59a0598497df8a5852bbf7377d421a5dbf643dea
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-09 19:57:14 +0900

    프래그먼트 생성

---
+++ app/src/main/java/com/sopt/now/presentation/main/MainFragment.kt
@@ -0,0 +1,27 @@
+package com.sopt.now.presentation.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sopt.now.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding
+        get() = requireNotNull(_binding)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
