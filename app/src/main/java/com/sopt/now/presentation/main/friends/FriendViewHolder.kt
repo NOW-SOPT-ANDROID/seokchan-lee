eb2eb0302f2d84a32ff5ae0466cdbe27c809bc4b
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-10 18:55:36 +0900

    뷰홀더 수정
    Friend데이터 클래스를 받는 onClick, onLongClick

--- app/src/main/java/com/sopt/now/presentation/main/friends/FriendViewHolder.kt
+++ app/src/main/java/com/sopt/now/presentation/main/friends/FriendViewHolder.kt
@@ -3,13 +3,34 @@ package com.sopt.now.presentation.main.friends
 import androidx.recyclerview.widget.RecyclerView
 import com.sopt.now.databinding.ItemFriendBinding
 
-class FriendViewHolder(private val binding: ItemFriendBinding) :
+class FriendViewHolder(
+    private val binding: ItemFriendBinding,
+    private val onClicked: (Friend) -> Unit,
+    private val onLongClicked: (Friend) -> Unit
+) :
     RecyclerView.ViewHolder(binding.root) {
-        fun onBind(friendData:Friend){
-            binding.run {
-                ivProfile.setImageResource(friendData.profileImage)
-                tvName.text = friendData.name
-                tvMusic.text = friendData.music
-            }
+    fun onBind(friendData: Friend) {
+        binding.run {
+            ivProfile.setImageResource(friendData.profileImage)
+            tvName.text = friendData.name
+            tvMusic.text = friendData.music
+
+            initRecyclerViewClickListener(friendData)
+            initRecyclerViewLongClickListener(friendData)
         }
+    }
+    private fun initRecyclerViewClickListener(friendData: Friend) {
+        binding.root.setOnClickListener {
+            onClicked(friendData)
+        }
+    }
+
+    private fun initRecyclerViewLongClickListener(friendData: Friend) {
+        binding.root.setOnLongClickListener {
+            onLongClicked(friendData)
+            return@setOnLongClickListener true
+        }
+    }
+
+
 }
