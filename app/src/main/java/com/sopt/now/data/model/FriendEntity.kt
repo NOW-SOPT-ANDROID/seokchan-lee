56b898bb6f4ae2a51657a6ce6683e0da13627631
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-11 16:27:22 +0900

    room FriendEntitiy추가

---
+++ app/src/main/java/com/sopt/now/data/model/FriendEntity.kt
@@ -0,0 +1,35 @@
+package com.sopt.now.data.model
+
+import androidx.annotation.DrawableRes
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "friend_table")
+data class FriendEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+    @DrawableRes val profileImage: Int,
+    val name: String,
+    val music: String
+) {
+    companion object {
+        fun toFriendEntity(friend: Friend) = run {
+            FriendEntity(
+                id = null,
+                profileImage = friend.profileImage,
+                name = friend.name,
+                music = friend.music
+
+            )
+        }
+
+        fun toFriend(friendEntityList:List<FriendEntity>) = friendEntityList.map { data->
+            Friend(
+                id = data.id,
+                profileImage = data.profileImage,
+                name = data.name,
+                music = data.music
+            )
+        }
+    }
+}
