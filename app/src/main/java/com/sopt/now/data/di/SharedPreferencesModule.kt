38cda688e488c24b689eab71b6de89686c37a330
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-04-20 19:28:31 +0900

    sharedpreferences모듈

---
+++ app/src/main/java/com/sopt/now/data/di/SharedPreferencesModule.kt
@@ -0,0 +1,20 @@
+package com.sopt.now.data.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object SharedPrefModule {
+    @Provides
+    @Singleton
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        return context.getSharedPreferences("user", Context.MODE_PRIVATE)
+    }
+}
