ced6f0c7cb1721ceb2524c4b46e511457a2a0f6d
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-05-02 19:04:32 +0900

    SignUp interface

---
+++ app/src/main/java/com/sopt/now/data/AuthService.kt
@@ -0,0 +1,12 @@
+package com.sopt.now.data
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("member/join")
+    fun signUp(
+        @Body request: RequestSignUpDto,
+    ): Call<ResponseSignUpDto>
+}
