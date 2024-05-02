fea223ae703efe780b514c8db6f08d932bab9708
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-05-03 02:09:34 +0900

    apiFactort 수정
    reqres url도 추가했습니다.

--- app/src/main/java/com/sopt/now/data/ApiFactory.kt
+++ app/src/main/java/com/sopt/now/data/ApiFactory.kt
@@ -2,25 +2,36 @@ package com.sopt.now.data
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.sopt.now.BuildConfig
+import com.sopt.now.BuildConfig.AUTH_BASE_URL
+import com.sopt.now.BuildConfig.REQRES_URL
 import com.sopt.now.data.model.service.AuthService
+import com.sopt.now.data.model.service.ReqresService
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 
 object ApiFactory {
-    private const val BASE_URL: String = BuildConfig.AUTH_BASE_URL
 
-    val retrofit: Retrofit by lazy {
+    val auth: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(AUTH_BASE_URL)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+    val reqres: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(REQRES_URL)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
 
-    inline fun <reified T> create(): T = retrofit.create(T::class.java)
+    inline fun <reified T> create(): T = auth.create(T::class.java)
+    inline fun <reified T> createReqres(): T = reqres.create(T::class.java)
+
 }
 
 object ServicePool {
     val authService = ApiFactory.create<AuthService>()
+    val reqresService = ApiFactory.createReqres<ReqresService>()
 }
