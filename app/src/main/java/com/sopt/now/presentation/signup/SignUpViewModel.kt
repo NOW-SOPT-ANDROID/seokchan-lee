8235fc75a109c375ce805f5f638dccf9336a77fa
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2024-05-02 19:06:32 +0900

    retrofit 회원가입
    ResponseSignUpDto(code=201, message=회원가입이 완료되었습니다.), userId: 307

--- app/src/main/java/com/sopt/now/presentation/signup/SignUpViewModel.kt
+++ app/src/main/java/com/sopt/now/presentation/signup/SignUpViewModel.kt
@@ -1,13 +1,19 @@
 package com.sopt.now.presentation.signup
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
-import com.sopt.now.MyApplication
 import com.sopt.now.R
 import com.sopt.now.data.PreferenceUtil
+import com.sopt.now.data.RequestSignUpDto
+import com.sopt.now.data.ResponseSignUpDto
+import com.sopt.now.data.ServicePool
 import com.sopt.now.presentation.model.User
 import dagger.hilt.android.lifecycle.HiltViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,35 +25,48 @@ class SignUpViewModel @Inject constructor(private val sharedPreferences: Prefere
     private val id: MutableLiveData<String> = MutableLiveData("")
     private val pw: MutableLiveData<String> = MutableLiveData("")
     private val nickname: MutableLiveData<String> = MutableLiveData("")
-    private val mbti: MutableLiveData<String> = MutableLiveData("")
+    private val phoneNumber: MutableLiveData<String> = MutableLiveData("")
 
-    fun setUserData() {
-        val userInfo = User(
-            id.value.toString(),
-            pw.value.toString(),
-            nickname.value.toString(),
-            mbti.value.toString()
-        )
-        val json = Gson().toJson(userInfo)
-        sharedPreferences.setString(PREF_KEY, json)
+    private val authService by lazy { ServicePool.authService }
+
+    fun signUp() {
+        val signUpRequest = getSignUpRequestDto()
+        authService.signUp(signUpRequest).enqueue(object : Callback<ResponseSignUpDto> {
+            override fun onResponse(
+                call: Call<ResponseSignUpDto>,
+                response: Response<ResponseSignUpDto>,
+            ) {
+                if (response.isSuccessful) {
+                    val data: ResponseSignUpDto? = response.body()
+                    val userId = response.headers()["location"]
+                    Log.d("SignUp", "data: $data, userId: $userId")
+                    //data: ResponseSignUpDto(code=201, message=회원가입이 완료되었습니다.), userId: 307
+                    _signUpState.value = SignUpState.Success(R.string.login_signup_success)
+                } else {
+                    _signUpState.value = SignUpState.Failure(R.string.signup_server_error)
+                }
+            }
+
+            override fun onFailure(
+                call: Call<ResponseSignUpDto>,
+                t: Throwable
+            ) {
+                _signUpState.value = SignUpState.Failure(R.string.signup_server_error)
+            }
+        })
     }
 
-    fun checkInvalidSignup() {
-        when {
-            nickname.value.toString().isBlank() -> _signUpState.value =
-                SignUpState.Failure(R.string.signup_empty_nickname)
-
-            mbti.value.toString().isBlank() -> _signUpState.value =
-                SignUpState.Failure(R.string.signup_empty_mbti)
-
-            id.value.toString().length !in 6..10 -> _signUpState.value =
-                SignUpState.Failure(R.string.signup_empty_id)
-
-            pw.value.toString().length !in 8..12 -> _signUpState.value =
-                SignUpState.Failure(R.string.signup_empty_pw)
-
-            else -> _signUpState.value = SignUpState.Success(R.string.login_signup_success)
-        }
+    private fun getSignUpRequestDto(): RequestSignUpDto {
+        val id = this.id.value.toString()
+        val password = this.pw.value.toString()
+        val nickname = this.nickname.value.toString()
+        val phoneNumber = this.phoneNumber.value.toString()
+        return RequestSignUpDto(
+            authenticationId = id,
+            password = password,
+            nickname = nickname,
+            phone = phoneNumber
+        )
     }
 
     fun updateId(msg: String) {
@@ -62,12 +81,7 @@ class SignUpViewModel @Inject constructor(private val sharedPreferences: Prefere
         nickname.value = msg
     }
 
-    fun updateMbti(msg: String) {
-        mbti.value = msg
-    }
-
-    companion object {
-        private const val PREF_KEY = "userData"
-
+    fun updatePhoneNumber(msg: String) {
+        phoneNumber.value = msg
     }
 }
